54a2769f81889ace77de2fd67225c4c7f6c78fe9
BlazeNade <noahfan220@gmail.com>
2026-08-20 10:27:18 +0800

    Updated player and entity

--- player.cpp
+++ player.cpp
@@ -2,10 +2,44 @@
 #include <cmath>
 #include <iostream>
 
+player::player() // added by Noah
+{
+	playerName = "Survivor";
+
+	setHealthPoints(100);
+	playerHealthPoints = getHealthPoints() + playerEndurance;
+
+	playerStrength = 2;
+	playerAgility = 2;
+	playerLuck = 2;
+	playerEndurance = 2;
+	playerIntelligence = 2;
+
+	playerStrengthFinal = playerStrength + playerLuckBoost();
+	playerAgilityFinal = playerAgility + playerLuckBoost();
+	playerLuckFinal = playerLuck;
+	playerEnduranceFinal = playerEndurance + playerLuckBoost();
+	playerIntelligenceFinal = playerIntelligence + playerLuckBoost();
+
+	playerActionPoints = playerAgility;
+
+	playerCombatMode = false;
+}
+
+float player::playerLuckBoost()
+{
+	return (playerLuckFinal / 8);
+}
+
 void player::move(int moveX, int moveY) {
 	//setPosition( getPosX() + moveX, getPosY() + moveY);
 	setPosX(getPosX() + moveX);
 	setPosY(getPosY() + moveY);
+
+	if (playerCombatMode == true)
+	{
+		playerActionPoints--;
+	}
 }
 
 void player::borderCol(int moveX, int moveY) {
@@ -24,7 +58,7 @@ void player::testAttack(enemy& targetenemy) {
 	int distX = getPosX() - targetenemy.getPosX();
 	int distY = getPosY() - targetenemy.getPosY();
 
-	int accuracy = 95;
+	float accuracy = 95;
 
 	//for diagonals, find hypotenuse of triangles with sides distX distY
 	float hypoDist = std::sqrt((distX * distX) + (distY * distY));
@@ -32,7 +66,7 @@ void player::testAttack(enemy& targetenemy) {
 	int a = static_cast<int>(hypoDist);
 
 	if (hypoDist > 2) {
-		accuracy = accuracy - (a * 2);
+		accuracy = accuracy - (a * (8 / playerStrengthFinal));
 	}
 	else {
 		accuracy = accuracy - a;
@@ -41,17 +75,55 @@ void player::testAttack(enemy& targetenemy) {
 	std::cout << "distance: " << hypoDist << ", accuracy: " << accuracy << std::endl;
 }
 
-void player::checkForEnemy(enemy& targetenemy)
+int player::getPlayerHealthPoints() // added by Noah
 {
-	//dstx and disty between enemy and player
-	int distX = getPosX() - targetenemy.getPosX();
-	int distY = getPosY() - targetenemy.getPosY();
-
-	//for diagonals, find hypotenuse of triangles with sides distX distY
-	float hypoDist = std::sqrt((distX * distX) + (distY * distY));
-
-	if (hypoDist <= 1.5) {
-		std::cout << "triggered battle cutscene with enemy" << std::endl;
-
-	}
+	return playerHealthPoints;
+}
+void setPlayerHealthPoints(int hp)
+{
+	playerHealthPoints = hp;
+}
+int player::getPlayerStrength() // added by Noah
+{
+	return playerStrength;
+}
+float player::getPlayerStrengthFinal() // added by Noah
+{
+	return playerStrengthFinal;
+}
+int player::getPlayerAgility()
+{
+	return playerAgility;
+}
+float player::getPlayerAgilityFinal() // added by Noah
+{
+	return playerAgilityFinal;
+}
+int player::getPlayerLuck()
+{
+	return playerLuck;
+}
+float player::getPlayerLuckFinal() // added by Noah
+{
+	return playerLuckFinal;
+}
+int player::getPlayerEndurance()
+{
+	return playerEndurance;
+}
+float player::getPlayerEnduranceFinal() // added by Noah
+{
+	return playerEnduranceFinal;
+}
+int player::getPlayerIntelligence()
+{
+	return playerIntelligence;
+}
+float player::getPlayerIntelligenceFinal() // added by Noah
+{
+	return playerIntelligenceFinal;
+}
+int player::getPlayerActionPoints()
+{
+	return playerActionPoints;
 }
