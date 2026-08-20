77dc1dcc1517c430808a1950dba953e59359d54d
Zavier <zavier08tan@gmail.com>
2026-08-20 14:56:15 +0800

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,14 @@
+#include "menu.h"
+#include <iostream>
+#include <conio.h> //char input
+#include <iomanip>
+
+menu::menu()
+{
+
+}
+
+void menu::menuOpen()
+{
+
+}
---
+++ menu.h
@@ -0,0 +1,16 @@
+#pragma once
+class menu
+{
+	//Objective
+	//Show time
+	//Show distance
+	//Show / edit name
+	//Restart game
+	//Restart stage
+
+public:
+	menu();
+	void menuOpen();
+
+};
+
