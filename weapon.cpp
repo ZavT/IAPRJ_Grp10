445a1a96f11099cdea9a19891fdf7e6d0113fed1
BlazeNade <noahfan220@gmail.com>
2026-08-24 10:31:21 +0800

    add boolean for if weapon is melee weapon and added melee damage boost

--- weapon.cpp
+++ weapon.cpp
@@ -1,13 +1,21 @@
 #include "weapon.h"
 
-weapon::weapon(std::string weaponname, int weaponprice, int weaponAP, int weapondmg, int weaponacc)
+weapon::weapon(std::string weaponname, int weaponprice, int weaponAP, int weapondmg, int weaponacc, bool isM)
 	: item(weaponname, weaponprice,  weaponAP){
 	damage = weapondmg;
 	accuracy = weaponacc;
+	isMelee = isM;
 }
 
-int weapon::getweapondmg(){
-	return damage;
+int weapon::getweapondmg(player& p){
+	if (isMelee)
+	{
+		return damage + static_cast<int>(p.getPlayerStrengthFinal());
+	}
+	else
+	{
+		return damage;
+	}
 }
 
 int weapon::getweaponacc(){
