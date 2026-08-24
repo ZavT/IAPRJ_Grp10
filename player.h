517be6bf59ddc26bfa89f81c5b4b23c916f77ceb
BlazeNade <noahfan220@gmail.com>
2026-08-24 10:03:13 +0800

    player take damage function

--- player.h
+++ player.h
@@ -59,4 +59,6 @@ public:
 	void borderCol(int moveX, int moveY, int maxBorderX, int maxBorderY);
 	void testAttack(enemy& targetenemy);
 	void checkForEnemy(enemy& targetenemy);
+
+	void takeDamage(int dmg);
 };
